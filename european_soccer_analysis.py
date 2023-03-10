3ee2e4ebaa4a6282c831fd13cbcdf8ddc58bad49
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2023-03-10 10:21:45 +0700

    Update european_soccer_analysis.py

--- european_soccer_analysis.py
+++ european_soccer_analysis.py
@@ -1,15 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import mysql.connector
 from mysql.connector import Error
-import sqlite3
 
 
 try:
-    mydb = mysql.connector.connect(host='localhost',
-                                         database='European_Soccer',
-                                         user='root',
-                                         password='12345678')
+    mydb = mysql.connector.connect(
+        host='localhost',
+        database='European_Soccer',
+        user='root',
+        password='12345678'
+    )
     if mydb.is_connected():
         db_Info = mydb.get_server_info()
+        mydb.start_transaction()
         print("Connected to MySQL Server version ", db_Info)
         eu_soccer_cursor = mydb.cursor()
         eu_soccer_cursor.execute("select database();")
@@ -21,177 +26,101 @@ except Error as e:
 finally:
     print("S->13 Session")
 
-conn = sqlite3.connect("database.sqlite")
-sqlite_cursor = conn.cursor()
+print("European_Soccer tables >>>>> ")
+tables = pd.read_sql("SHOW TABLES;", mydb)
+print(tables)
 
-# Create Country Table
-sql_stmt = """CREATE TABLE IF NOT EXISTS `Country` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT, 
-    `name` VARCHAR(255) UNIQUE
-)"""
-eu_soccer_cursor.execute(sql_stmt)
-eu_soccer_cursor.execute("SELECT COUNT(*) from Country")
-row_numbers, = eu_soccer_cursor.fetchone()
-if not row_numbers:
-    sql_stmt = "INSERT INTO Country (id, name) VALUES (%s, %s)"
-    eu_soccer_cursor.executemany(sql_stmt, list(sqlite_cursor.execute("SELECT * FROM Country")))
+print("Country Table >>>>> ")
+tables = pd.read_sql("SELECT * FROM Country", mydb)
+print(tables)
 
-# Create League Table
-sql_stmt = """CREATE TABLE IF NOT EXISTS `League` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `country_id` INTEGER, 
-    `name` VARCHAR(255) UNIQUE, 
-    FOREIGN KEY(`country_id`) REFERENCES `country`(`id`)
-)"""
-eu_soccer_cursor.execute(sql_stmt)
-eu_soccer_cursor.execute("SELECT COUNT(*) from League")
-row_numbers, = eu_soccer_cursor.fetchone()
-if not row_numbers:
-    sql_stmt = "INSERT INTO League (id, country_id, name) VALUES (%s, %s, %s)"
-    eu_soccer_cursor.executemany(sql_stmt, list(sqlite_cursor.execute("SELECT * FROM League")))
+print("List of League >>>>>>>>>>> ")
+leagues = pd.read_sql("""SELECT *
+                        FROM League
+                        JOIN Country ON Country.id = League.country_id;""", mydb)
+print(leagues)
+#
+print("List of Team >>>>>>>>>>>>")
+teams = pd.read_sql("""SELECT *
+                        FROM Team
+                        ORDER BY team_long_name
+                        LIMIT 10;""", mydb)
+print(teams)
+#
+print("List of Match >>>>>>>>>>>")
+detailed_matches = pd.read_sql("""SELECT `Match`.id,
+                                        Country.name,
+                                        League.name,
+                                        season,
+                                        stage,
+                                        date,
+                                        HT.team_long_name AS  home_team,
+                                        AT.team_long_name AS away_team,
+                                        home_team_goal,
+                                        away_team_goal
+                                FROM `Match`
+                                JOIN Country on Country.id = `Match`.country_id
+                                JOIN League on League.id = `Match`.league_id
+                                LEFT JOIN Team AS HT on HT.team_api_id = `Match`.home_team_api_id
+                                LEFT JOIN Team AS AT on AT.team_api_id = `Match`.away_team_api_id
+                                WHERE Country.name = 'Spain'
+                                ORDER by date
+                                LIMIT 10;""", mydb)
+print(detailed_matches)
+#
+print("League by season >>>>>>>>>>>>>>>> ")
+leagues_by_season = pd.read_sql("""SELECT Country.name as country_name,
+                                        League.name as league_name,
+                                        season,
+                                        count(distinct stage) AS number_of_stages,
+                                        count(distinct HT.team_long_name) AS number_of_teams,
+                                        avg(home_team_goal) AS avg_home_team_scors,
+                                        avg(away_team_goal) AS avg_away_team_goals,
+                                        avg(home_team_goal-away_team_goal) AS avg_goal_dif,
+                                        avg(home_team_goal+away_team_goal) AS avg_goals,
+                                        sum(home_team_goal+away_team_goal) AS total_goals
+                                FROM `Match`
+                                JOIN Country on Country.id = `Match`.country_id
+                                JOIN League on League.id = `Match`.league_id
+                                LEFT JOIN Team AS HT on HT.team_api_id = `Match`.home_team_api_id
+                                LEFT JOIN Team AS AT on AT.team_api_id = `Match`.away_team_api_id
+                                WHERE Country.name in ('Spain', 'Germany', 'France', 'Italy', 'England')
+                                GROUP BY Country.name, League.name, season
+                                HAVING count(distinct stage) > 10
+                                ORDER BY Country.name, League.name, season DESC
+                                ;""", mydb)
+print(leagues_by_season)
+#
+df = pd.DataFrame(index=np.sort(leagues_by_season['season'].unique()), columns=leagues_by_season['country_name'].unique())
+df.loc[:,'Germany'] = list(leagues_by_season.loc[leagues_by_season['country_name']=='Germany','avg_goals'])
+df.loc[:,'Spain']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='Spain','avg_goals'])
+df.loc[:,'France']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='France','avg_goals'])
+df.loc[:,'Italy']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='Italy','avg_goals'])
+df.loc[:,'England']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='England','avg_goals'])
+df.plot(figsize=(12,5),title='Average Goals per Game Over Time')
+plt.xlabel("Times")
+plt.ylabel("average goals")
+plt.show()
 
-# Create Player Table
-sql_stmt = """CREATE TABLE IF NOT EXISTS `Player` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `player_api_id`	INTEGER UNIQUE,
-    `player_name` TEXT,
-    `player_fifa_api_id` INTEGER UNIQUE,
-    `birthday` TEXT,
-    `height` INTEGER,
-    `weight` INTEGER
-)"""
-eu_soccer_cursor.execute(sql_stmt)
-eu_soccer_cursor.execute("SELECT COUNT(*) from Player")
-row_numbers, = eu_soccer_cursor.fetchone()
-if not row_numbers:
-    sql_stmt = """INSERT INTO Player (id, player_api_id, player_name, player_fifa_api_id, birthday, height, weight)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-    """
-    eu_soccer_cursor.executemany(sql_stmt, list(sqlite_cursor.execute("SELECT * FROM Player")))
+df = pd.DataFrame(index=np.sort(leagues_by_season['season'].unique()), columns=leagues_by_season['country_name'].unique())
+df.loc[:,'Germany'] = list(leagues_by_season.loc[leagues_by_season['country_name']=='Germany','avg_goal_dif'])
+df.loc[:,'Spain']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='Spain','avg_goal_dif'])
+df.loc[:,'France']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='France','avg_goal_dif'])
+df.loc[:,'Italy']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='Italy','avg_goal_dif'])
+df.loc[:,'England']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='England','avg_goal_dif'])
+df.plot(figsize=(12,5),title='Average Goals Diff per Game Over Time')
+plt.xlabel("Times")
+plt.ylabel("avg_goal_dif")
+plt.show()
 
-# Create Player_Attributes Table
-sql_stmt = """
-CREATE TABLE IF NOT EXISTS `Player_Attributes` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `player_fifa_api_id` INTEGER,
-    `player_api_id`	INTEGER,
-    `date` TEXT,
-    `overall_rating` INTEGER,
-    `potential`	INTEGER,
-    `preferred_foot` TEXT,
-    `attacking_work_rate` TEXT,
-    `defensive_work_rate` TEXT,
-    `crossing` INTEGER,
-    `heading_accuracy` INTEGER,
-    `finishing`	INTEGER,
-    `short_passing`	INTEGER,
-    `volleys` INTEGER,
-    `dribbling`	INTEGER,
-    `curve`	INTEGER,
-    `free_kick_accuracy` INTEGER,
-    `long_passing` INTEGER,
-    `ball_control` INTEGER,
-    `acceleration` INTEGER,
-    `sprint_speed` INTEGER,
-    `agility` INTEGER,
-    `reactions` INTEGER,
-    `balance` INTEGER,
-    `shot_power` INTEGER,
-    `jumping` INTEGER,
-    `stamina` INTEGER,
-    `strength` INTEGER,
-    `long_shots` INTEGER,
-    `aggression` INTEGER,
-    `interceptions`	INTEGER,
-    `positioning` INTEGER,
-    `vision` INTEGER,
-    `penalties`	INTEGER,
-    `marking` INTEGER,
-    `standing_tackle` INTEGER,
-    `sliding_tackle` INTEGER,
-    `gk_diving`	INTEGER,
-    `gk_handling` INTEGER,
-    `gk_kicking` INTEGER,
-    `gk_positioning` INTEGER,
-    `gk_reflexes` INTEGER,
-    FOREIGN KEY(`player_fifa_api_id`) REFERENCES `Player`(`player_fifa_api_id`),
-    FOREIGN KEY(`player_api_id`) REFERENCES `Player`(`player_api_id`)
-)"""
-eu_soccer_cursor.execute(sql_stmt)
-eu_soccer_cursor.execute("SELECT COUNT(*) from Player_Attributes")
-row_numbers, = eu_soccer_cursor.fetchone()
-if not row_numbers:
-    sql_stmt = """
-    INSERT INTO Player_Attributes (
-        id,
-        player_fifa_api_id,
-        player_api_id,
-        date,
-        overall_rating,
-        potential,
-        preferred_foot,
-        attacking_work_rate,
-        defensive_work_rate,
-        crossing,
-        heading_accuracy,
-        finishing,
-        short_passing,
-        volleys,
-        dribbling,
-        curve,
-        free_kick_accuracy,
-        long_passing,
-        ball_control,
-        acceleration,
-        sprint_speed,
-        agility,
-        reactions,
-        balance,
-        shot_power,
-        jumping,
-        stamina,
-        strength,
-        long_shots,
-        aggression,
-        interceptions,
-        positioning,
-        vision,
-        penalties,
-        marking,
-        standing_tackle,
-        sliding_tackle,
-        gk_diving,
-        gk_handling,
-        gk_kicking,
-        gk_positioning,
-        gk_reflexes)
-    VALUES (%s, %s, %s, %s, %s, %s,
-            %s, %s, %s, %s, %s, %s,
-            %s, %s, %s, %s, %s, %s,
-            %s, %s, %s, %s, %s, %s,
-            %s, %s, %s, %s, %s, %s,
-            %s, %s, %s, %s, %s, %s,
-            %s, %s, %s, %s, %s, %s)
-    """
-    eu_soccer_cursor.execute("SET GLOBAL FOREIGN_KEY_CHECKS=0;")
-    eu_soccer_cursor.executemany(sql_stmt, list(sqlite_cursor.execute("SELECT * FROM Player_Attributes")))
-    eu_soccer_cursor.execute("SET GLOBAL FOREIGN_KEY_CHECKS=1;")
+df = pd.DataFrame(index=np.sort(leagues_by_season['season'].unique()), columns=leagues_by_season['country_name'].unique())
+df.loc[:,'Germany'] = list(leagues_by_season.loc[leagues_by_season['country_name']=='Germany','total_goals'])
+df.loc[:,'Spain']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='Spain','total_goals'])
+df.loc[:,'France']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='France','total_goals'])
+df.loc[:,'Italy']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='Italy','total_goals'])
+df.loc[:,'England']   = list(leagues_by_season.loc[leagues_by_season['country_name']=='England','total_goals'])
+df.plot(figsize=(12,5),title='Total Goals per Game Over Time')
 
-# Create Team Table
-sql_stmt = """CREATE TABLE IF NOT EXISTS `Team` (
-    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `team_api_id` INTEGER UNIQUE,
-    `team_fifa_api_id` INTEGER,
-    `team_long_name` TEXT,
-    `team_short_name` TEXT
-)"""
-eu_soccer_cursor.execute(sql_stmt)
-eu_soccer_cursor.execute("SELECT COUNT(*) from Team")
-row_numbers, = eu_soccer_cursor.fetchone()
-if not row_numbers:
-    sql_stmt = """INSERT INTO Team (id, team_api_id, team_fifa_api_id, team_long_name, team_short_name)
-            VALUES (%s, %s, %s, %s, %s)
-        """
-    eu_soccer_cursor.executemany(sql_stmt, list(sqlite_cursor.execute("SELECT * FROM Team")))
-
-mydb.commit()
+plt.xlabel("Times")
+plt.ylabel("total_goals")
+plt.show()
